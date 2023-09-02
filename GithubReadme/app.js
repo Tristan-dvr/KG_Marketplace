c23d9bdea042f4b763a6be41a3994e7a88f3bb86
KG <war3spells@gmail.com>
2023-09-02 23:35:03 +0300

    node changes

--- GithubReadme/app.js
+++ GithubReadme/app.js
@@ -1,16 +1,24 @@
-var argumentFileName = process.argv[2];
-var argumentNewFileName = process.argv[3];
+var currentFolder = __dirname;
+var argumentFolderName = process.argv[2];
 var md = require('markdown-it')({
     html: true,
     linkify: true,
     typographer: true
 });
 const fs = require('fs');
-var data = fs.readFileSync(argumentFileName+".md", 'utf8');
-var result = md.render(data);
-fs.writeFile(argumentNewFileName+".md", result, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
+if (!fs.existsSync(argumentFolderName)) {
+    fs.mkdirSync(argumentFolderName);
+}
+var allFilesInCurrentFolder = fs.readdirSync(currentFolder).filter(file => file.endsWith('.md'));
+allFilesInCurrentFolder.forEach(function (file) {
+    var data = fs.readFileSync(file, 'utf8');
+    var result = md.render(data);
+    var filePath= currentFolder + "\\" + argumentFolderName + "\\" + file;
+    fs.writeFile(filePath, result, function (err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log(`Parsed ${filePath}`);
+    });
 });
+
