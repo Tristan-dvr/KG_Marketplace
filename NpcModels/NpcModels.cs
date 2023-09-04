60e163fc9a02c8cea13b891eb9cdd8e971acf191
KG <war3spells@gmail.com>
2023-09-04 23:19:55 +0300

    models

--- NpcModels/NpcModels.cs
+++ NpcModels/NpcModels.cs
@@ -11,14 +11,14 @@ using UnityEngine;
 
 namespace NpcModels
 {
-    [BepInPlugin(GUID,GUID,VERSION)]
+    [BepInPlugin(GUID, GUID, VERSION)]
     public class NpcModels : BaseUnityPlugin
     {
         private const string GUID = "kg.MarketplaceNpcModels";
         private const string VERSION = "1.0.0";
 
         private static List<GameObject> ToLoad;
-        
+
         private static AssetBundle GetAssetBundle(string filename)
         {
             Assembly execAssembly = Assembly.GetExecutingAssembly();
@@ -26,18 +26,27 @@ namespace NpcModels
             using Stream stream = execAssembly.GetManifestResourceStream(resourceName)!;
             return AssetBundle.LoadFromStream(stream);
         }
-        
+
         private void Awake()
         {
             ToLoad = GetAssetBundle("npcmodels").LoadAllAssets<GameObject>().ToList();
             new Harmony(GUID).PatchAll();
         }
-        
-        [HarmonyPatch(typeof(ZNetScene),nameof(ZNetScene.Awake))]
+
+        [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
         private static class ZNetScene_Awake_Patch
         {
             [UsedImplicitly]
-            private static void Postfix(ZNetScene __instance) => ToLoad.ForEach(p => __instance.m_namedPrefabs[p.name.GetStableHashCode()] = p);
+            private static void Postfix(ZNetScene __instance)
+            {
+                ToLoad.ForEach(p =>
+                {
+                    __instance.m_namedPrefabs[p.name.GetStableHashCode()] = p;
+                    IEnumerable<Renderer> mesh = p.GetComponentsInChildren<MeshRenderer>(true).Cast<Renderer>().Concat(p.GetComponentsInChildren<SkinnedMeshRenderer>());
+                    foreach (Material material in mesh.SelectMany(m => m.materials))
+                        material.shader = Shader.Find("Custom/Creature");
+                });
+            }
         }
     }
 }
